2ca0639871e6361c77ba8c0af1bc08128b7bc957
dohd <alloicebrian@gmail.com>
2023-07-02 08:52:11 +0300

    Add Password Change Modal

---
+++ screens/PasswordModal.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Modal } from "react-native";
+import { Button, Text, TextInput, Divider, HStack, VStack } from "@react-native-material/core";
+import { Formik } from "formik";
+
+export function PasswordModal(props) {
+  return (
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={props.passwordModalVisible}
+        onRequestClose={() => {
+          props.setPasswordModalVisible(!props.passwordModalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.mainCardView}>
+            <View style={{ flexDirection: "column", alignItems: "center", marginLeft: 10, marginRight: 10 }}>
+              <Text variant="h6" color="gray" style={{ marginBottom: 20 }}>Update Password</Text>
+              <FormInput {...props} />
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+}
+
+function FormInput(props) {
+  return (
+    <Formik
+      initialValues={{ password: "", username: "" }}
+      onSubmit={(values) => console.log(values)}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <VStack spacing={20}>
+          <TextInput
+            label="Current Password*"
+            variant="standard"
+            style={{ fontSize: 20 }}
+            // onChangeText={handleChange('password')}
+            // onBlur={handleBlur('password')}
+            // value={values.password}
+          />
+          <TextInput
+            label="New Password*"
+            variant="standard"
+            style={{ fontSize: 20 }}
+            // onChangeText={handleChange('password')}
+            // onBlur={handleBlur('password')}
+            // value={values.password}
+          />
+
+
+          <TextInput
+            label="Confirm Password*"
+            variant="standard"
+            style={{ fontSize: 20 }}
+            // onChangeText={handleChange('password')}
+            // onBlur={handleBlur('password')}
+            // value={values.password}
+          />
+
+          <HStack spacing={40} style={{ marginTop: 40 }}>
+            <Button
+              title="Cancel"
+              variant="outlined"
+              style={{ width: "40%" }}
+              onPress={() => {
+                // handleSubmit();
+                props.setPasswordModalVisible(false);
+                // props.navigation.navigate("Settings");
+              }}
+            />
+            <Button
+              title="Save"
+              style={{ width: "40%" }}
+              onPress={() => {
+                // handleSubmit();
+                props.setPasswordModalVisible(false);
+                // props.navigation.navigate("Settings");
+              }}
+            />
+
+          </HStack>
+        </VStack>
+      )}
+    </Formik>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainCardView: {
+    height: 400,
+    backgroundColor: "white",
+    borderRadius: 15,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 1,
+    shadowRadius: 8,
+    elevation: 8,
+    marginTop: 16,
+    justifyContent: "center",
+  },
+  // modal
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+  },
+});
--- screens/SettingsMenuScreen.jsx
+++ screens/SettingsMenuScreen.jsx
@@ -1,80 +1,78 @@
-import React from "react";
-import { View, StyleSheet } from 'react-native';
-import { 
-    AppBar, 
-    ListItem,
-    IconButton,
-} from "@react-native-material/core";
+import React, {useState} from "react";
+import { View, StyleSheet } from "react-native";
+import { AppBar, ListItem, IconButton, HStack, VStack, Divider } from "@react-native-material/core";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import { PasswordModal } from "./PasswordModal";
 
-
-export default function SettingsMenuScreen({navigation, route}) {
+export default function SettingsMenuScreen({ navigation, route }) {
+  const [passwordModalVisible, setPasswordModalVisible] = useState(false);
   return (
-    <View>
+    <>
       <AppBar
-        title="Settings Menu"
-        leading={props => (
+        title="Settings"
+        leading={(props) => (
           <IconButton
-              onPress={() => navigation.navigate('Home')}
-              icon={props => <Icon name="keyboard-backspace" {...props} />}
-              {...props}
+            onPress={() => navigation.navigate("Home")}
+            icon={(props) => <Icon name="keyboard-backspace" {...props} />}
+            {...props}
           />
         )}
       />
+
       <View style={styles.mainCardView}>
-          <View>
-            <ListItem
-              title="Profile"
-              leading={<Icon name="account-circle" size={24} />}
-              trailing={props => <Icon name="chevron-right" {...props} />}
-            />
-            <ListItem
-              title="Security"
-              leading={<Icon name="account-lock" size={24} />}
-              trailing={props => <Icon name="chevron-right" {...props} />}
-            />
-            <ListItem
-              title="Logout"
-              leading={<Icon name="logout-variant" size={24} />}
-              // trailing={props => <Icon name="chevron-right" {...props} />}
-              onPress={() => {
-                navigation.navigate('Login');
-                navigation.reset({index: 0, routes: [{ name: 'Login' }]})
-              }}
-            />
-            {/* <ListItem
-              title="Trash"
-              trailing={props => <Icon name="chevron-right" {...props} />}
-            />
-            <ListItem
-              title="Spam"
-              trailing={props => <Icon name="chevron-right" {...props} />}
-            /> */}
-          </View>
-      
+        <VStack>
+          
+          <ListItem
+            title="Username"
+            secondaryText="Super Admin"
+            leading={<Icon name="account-circle" size={24} />}
+            trailing={(props) => <Icon name="chevron-right" {...props} />}
+          />
+          <ListItem
+            title="Phone Number"
+            secondaryText="0788222400"
+            leading={<Icon name="phone" size={24} />}
+            trailing={(props) => <Icon name="chevron-right" {...props} />}
+          />
+          <ListItem
+            title="Password"
+            secondaryText="******"
+            leading={<Icon name="lock" size={24} />}
+            trailing={(props) => <Icon name="chevron-right" {...props} />}
+            onPress={() => {
+              setPasswordModalVisible(true);
+              // navigation.navigate("Login");
+              // navigation.reset({ index: 0, routes: [{ name: "Login" }] });
+            }}
+          />
+          
+          <ListItem
+            title="Logout"
+            leading={<Icon name="logout-variant" size={24} />}
+            onPress={() => {
+              navigation.navigate("Login");
+              navigation.reset({ index: 0, routes: [{ name: "Login" }] });
+            }}
+          />    
+
+          <PasswordModal passwordModalVisible={passwordModalVisible} setPasswordModalVisible={setPasswordModalVisible} />      
+        </VStack>
       </View>
-    </View>
+    </>
   );
-};
+}
 
 const styles = StyleSheet.create({
   mainCardView: {
-    height: 300,
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    borderRadius: 5,
-    shadowOffset: {width: 0, height: 0},
+    height: 400,
+    backgroundColor: "white",
+    shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 1,
     shadowRadius: 8,
     elevation: 8,
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    paddingTop: 16,
-    paddingLeft: 16,
-    paddingRight: 16,
+    marginLeft: 10,
+    marginRight: 10,
     marginTop: 10,
-    marginBottom: 4,
-    marginLeft: 8,
-    marginRight: 8,
+    borderRadius: 20
   },
 });
